3601298df3e7104c4d448bfd209b804049d71c20
HikaruSato <110364092+Omochi800@users.noreply.github.com>
2022-10-13 12:58:43 +0000

    [3.1.2]

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,26 @@
 class Admin::ItemsController < ApplicationController
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_items_path
+  end
+
+  def index
+    @items = Item.all
+  end
+
+  def show
+    
+
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name,:image,:introduction)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,31 @@
 Rails.application.routes.draw do
+  namespace :public do
+    root to:'homes#top'
+    resources :items
+    resources :registrations
+    resources :customers
+    resources :cart_items
+    resources :orders
+    resources :destinations
+    
+  end
+  namespace :admin do
+    resources :items
+    resources :genres
+    resources :cutomers
+    resources :orders
+    resources :order_details
+    
+  end
   devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
 
+
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
