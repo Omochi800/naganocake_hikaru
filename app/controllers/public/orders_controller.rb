f840b1d364e0ddce9397a75e7b892b33a875f5c2
HikaruSato <110364092+Omochi800@users.noreply.github.com>
2022-11-20 07:25:36 +0000

    202211201625

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -27,18 +27,28 @@ def complete
 end
 
 def confirm
-    @order = Order.new(order_params)
+    @order = Order.new
+if params[:order][:select_address] == "0"
+   @order.postal_code = current_customer.postal_code
+   @order.address = current_customer.address
+   @order.name = current_customer.first_name + current_customer.last_name
+elsif params[:order][:select_address] == "1"
     @destination = Destination.find(params[:order][:address_id])
     @order.postal_code = @destination.postal_code
     @order.address = @destination.address
     @order.name = @destination.name
-end
+elsif params[:order][:select_address] == "2"
+    @order = Order.new(order_params)
 
+end
+end
 
 private
 def order_params
   params.require(:order).permit(:payment_method,:postal_code,:address,:name)
 end
+def destination_params
+  params.require(:destination).permit(:address_id,:select_address)
 end
-
+end
 
--- app/models/destination.rb
+++ app/models/destination.rb
@@ -2,6 +2,6 @@ class Destination < ApplicationRecord
   belongs_to:customer
 
   def destination_display
-    "〒" + postal_code + '' + address + '' + name
+    '〒' + postal_code + '' + address + '' + name
   end
 end
